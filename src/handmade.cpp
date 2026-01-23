f2578d12d01b3ba24ba0005e2d8455ee201666c0
Pablo Peña <pablo_penna7@hotmail.com>
2026-01-23 21:49:43 +0800

    experimenting with showing available drivers for renderer and claiming window for GPU

--- src/handmade.cpp
+++ src/handmade.cpp
@@ -1,12 +1,20 @@
 #include <SDL3/SDL.h>
 #include <SDL3/SDL_error.h>
 #include <SDL3/SDL_events.h>
+#include "SDL3/SDL_gpu.h"
 #include <SDL3/SDL_init.h>
 #include <SDL3/SDL_render.h>
 #include <SDL3/SDL_video.h>
 
+#include <stdbool.h>
 #include <stdio.h>
 #include <stdlib.h>
+
+#define persistent_variable static
+#define global_variable static
+#define private_function static
+
+global_variable bool isRunning;
 
 bool handleEvent(SDL_Event *event) {
     bool shouldQuit = false;
@@ -25,20 +33,20 @@ bool handleEvent(SDL_Event *event) {
             int newHeight = ((SDL_WindowEvent*)event)->data2;
             printf("Resized to %d x %d\n", newWidth, newHeight);
         } break;
-        
+
         case SDL_EVENT_WINDOW_EXPOSED:
         {
             SDL_Window *window = SDL_GetWindowFromID(event->window.windowID);
             SDL_Renderer *renderer = SDL_GetRenderer(window);
             static bool isWhite = true;
-            
+
             if(isWhite) {
                 SDL_SetRenderDrawColor(renderer, 255, 255, 255, 255);
             } else {
                 SDL_SetRenderDrawColor(renderer, 0, 0, 0, 255);
             }
             isWhite = !isWhite;
-            
+
             SDL_RenderClear(renderer);
             SDL_RenderPresent(renderer);
         } break;
@@ -54,6 +62,21 @@ void handleFatalError() {
     SDL_Quit();
 
     exit((unsigned char)255);
+}
+
+void showAvailableDrivers() {
+    int numDrivers = SDL_GetNumRenderDrivers();
+    printf("Nº available drivers: %d\n", numDrivers);
+    for(int i=0;i<numDrivers;i++) {
+        const char* info = SDL_GetRenderDriver(i);
+        printf("Available driver: %s\n", info);
+    }
+}
+
+void claimWindowForGpu(SDL_Window *window) {
+    SDL_GPUDevice *gpuDevice = SDL_CreateGPUDevice(SDL_GPU_SHADERFORMAT_PRIVATE | SDL_GPU_SHADERFORMAT_SPIRV, true, 0);
+    bool success = SDL_ClaimWindowForGPUDevice(gpuDevice, window);
+    printf("was successful claiming window for GPU: %b\n", success);
 }
 
 int main(int argc, char *argv[]) {
@@ -76,10 +99,15 @@ int main(int argc, char *argv[]) {
         handleFatalError();
     }
 
+    showAvailableDrivers();
+
+    claimWindowForGpu(window);
+
     SDL_Renderer *renderer = SDL_CreateRenderer(window, 0);
     if(!renderer) {
         handleFatalError();
     }
+    const char* render_name = SDL_GetRendererName(renderer);
 
     for(;;) {
         SDL_Event event;
